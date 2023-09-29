df3e01d2a963b983f5b64d0a60676f9530c0604f
w0826103 <124601041+w0826103@users.noreply.github.com>
2023-09-29 19:06:04 -0400

    Add files via upload

---
+++ web303_assignment2/js/main.js
@@ -0,0 +1,27 @@
+// Task : Assignment 2
+// Name :Gurbinderjeet Kaur
+// S-ID : 0826103
+
+
+$(document).ready(function()
+{
+
+    $("#prospect").click(function()
+    {
+
+        $("#content").css("height","500");
+        $('div#content').load("prospect.html").hide().slideToggle("fast");
+    });
+    
+    $("#convert").click(function()
+    {
+        $("#content").css("height","500");
+
+        $('div#content').load("convert.html").hide().slideToggle("fast");
+    });
+     $("#retain").click(function()
+    {
+        $("#content").css("height","500");
+        $('div#content').load("retain.html").hide().slideToggle("fast");
+    });
+});
